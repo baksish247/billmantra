28a8aaa7a85b60b82b2a2596157aa76474eb7d2f
SUDIP-69 <csudip2311@gmail.com>
2025-06-10 23:38:57 +0530

    Remove deprecated configuration files and update dependencies. Downgrade Next.js to version 14.1.0 and Tailwind CSS to version 3.4.1. Update global styles to use Tailwind's new syntax. Change font imports in layout component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import "./globals.css";
 import { ReduxProvider } from "./redux/providers/ReduxProvider";
 import BottomNavigation from "./components/bottom-navigation";
 import QueryClientProviderWrapper from "./redux/providers/QueryClientProvider";
 import { Toaster } from "sonner";
 
-const geistSans = Geist({
+const geistSans = Inter({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
+const geistMono = Roboto_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
